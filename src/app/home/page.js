d3044ea6c4f69ec9e1134a2b2709cb43ef854741
Mariela Nava <Mariela.NavaR@ibm.com>
2025-04-17 12:04:40 -0500

    feat(tutorial): complete step 4

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -104,23 +104,25 @@ export default function LandingPage() {
           </TabPanels>
         </Tabs>
       </Column>
-      <InfoSection heading="The Principles">
-        <InfoCard
-          heading="Carbon is Open"
-          body="It's a distributed effort, guided by the principles of the open-source movement. Carbon's users are also it's makers, and everyone is encouraged to contribute."
-          icon={() => <Advocate size={32} />}
-        />
-        <InfoCard
-          heading="Carbon is Modular"
-          body="Carbon's modularity ensures maximum flexibility in execution. It's components are designed to work seamlessly with each other, in whichever combination suits the needs of the user."
-          icon={() => <AcceleratingTransformation size={32} />}
-        />
-        <InfoCard
-          heading="Carbon is Consistent"
-          body="Based on the comprehensive IBM Design Language, every element and component of Carbon was designed from the ground up to work elegantly together to ensure consistent, cohesive user experiences."
-          icon={() => <Globe size={32} />}
-        />
-      </InfoSection>
+      <Column lg={16} md={8} sm={4} className="landing-page__r3">
+        <InfoSection heading="The Principles">
+          <InfoCard
+            heading="Carbon is Open"
+            body="It's a distributed effort, guided by the principles of the open-source movement. Carbon's users are also it's makers, and everyone is encouraged to contribute."
+            icon={() => <Advocate size={32} />}
+          />
+          <InfoCard
+            heading="Carbon is Modular"
+            body="Carbon's modularity ensures maximum flexibility in execution. It's components are designed to work seamlessly with each other, in whichever combination suits the needs of the user."
+            icon={() => <AcceleratingTransformation size={32} />}
+          />
+          <InfoCard
+            heading="Carbon is Consistent"
+            body="Based on the comprehensive IBM Design Language, every element and component of Carbon was designed from the ground up to work elegantly together to ensure consistent, cohesive user experiences."
+            icon={() => <Globe size={32} />}
+          />
+        </InfoSection>
+      </Column>
     </Grid>
   );
 }
